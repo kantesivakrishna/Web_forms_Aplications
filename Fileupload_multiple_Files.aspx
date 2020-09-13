257be031d305fe2dd929f587cd46008bf2168722
sivakrishna k <sivakrishna.kante@outlook.com>
2020-09-13 08:18:49 +0530

    Multiple files upload

---
+++ Fileupload_multiple_Files.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Fileupload_multiple_Files.aspx.cs" Inherits="Web_forms_Aplications.Fileupload_multiple_Files" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>multiplefiles_upload</title>
+</head>
+<body>
+    <h1>select the files you want to upload
+
+    </h1>
+    <form id="form1" runat="server">
+        <div>
+            <asp:FileUpload ID="FileUpload_01" runat="server" AllowMultiple="true"/>
+            
+        </div>
+        <p>
+
+            <asp:Button ID="Submit" runat="server" Text="UploadFile" OnClick="Submit_Click"/>
+        </p>
+    </form>
+    <asp:Label runat="server" ID="FileUploadStatus"></asp:Label>
+</body>
+</html>
---
+++ Fileupload_multiple_Files.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web_forms_Aplications
+{
+    public partial class Fileupload_multiple_Files : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            if ((FileUpload_01 != null) && (FileUpload_01.PostedFile.ContentLength > 0))
+            {
+                var count = 0;
+                foreach (HttpPostedFile uploadfile in FileUpload_01.PostedFiles)
+                {
+                    string fn = System.IO.Path.GetFileName(uploadfile.FileName);
+                    string SaveLocation = Server.MapPath("upload") + "\\" + fn;
+                    try
+                    {
+                        uploadfile.SaveAs(SaveLocation);
+                        count++;
+                    }
+                    catch (Exception ex)
+                    {
+                        FileUploadStatus.Text = "Error:" + ex.Message;
+                    }
+                }
+                if (count > 0)
+                {
+                    FileUploadStatus.Text = count + "\tFiles has been uploaded.";
+                }
+                else
+                {
+                    FileUploadStatus.Text = "Please select a file to upload.";
+                }
+            }
+
+        }
+    }
+}
