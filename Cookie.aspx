994a6037fbff936e38fda4b82c82aadc3ab9aba9
sivakrishna k <sivakrishna.kante@outlook.com>
2020-09-14 05:24:57 +0530

    cookie

---
+++ Cookie.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Cookie.aspx.cs" Inherits="Web_forms_Aplications.Cookie" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>cookies</title>
+</head>
+<body>
+    <h1>cookie samples</h1>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Lable1" runat="server" Text="Lable"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Cookie.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web_forms_Aplications
+{
+    public partial class Cookie : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HttpCookie cookie = new HttpCookie("krishna");
+            cookie.Value = "sivakrishna";
+            Response.Cookies.Add(cookie);
+
+            var co_val = Response.Cookies["krishna"].Value;
+            Lable1.Text = co_val;
+
+
+        }
+    }
+}
