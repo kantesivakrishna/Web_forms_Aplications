f21fde575448bdd95379c0d0fcb953233b7d27a7
sivakrishna k <sivakrishna.kante@outlook.com>
2020-09-13 19:51:19 +0530

    File_DownLoad

---
+++ File_DownLoad.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="File_DownLoad.aspx.cs" Inherits="Web_forms_Aplications.File_DownLoad" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>DownLoadFiles</title>
+</head>
+<body>
+    <h1>click here to download the file available</h1>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="Download" runat="server" OnClick="Download_Click" Text="DownloadFile"/>
+        </div>
+    </form>
+    <asp:Label  ID="Lable1" runat="server"></asp:Label>
+</body>
+</html>
---
+++ File_DownLoad.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web_forms_Aplications
+{
+    public partial class File_DownLoad : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Download_Click(object sender, EventArgs e)
+        {
+            string FilePath = "C:\\Users\\DELL\\Desktop\\Sample.txt";
+            FileInfo File = new FileInfo(FilePath);
+
+            if (File.Exists)
+            {
+                Response.Clear();
+                Response.AddHeader("Content-Disposition", "attachment;FlieName" + File.Name);
+                Response.AddHeader("Content-Length",File.Length.ToString());
+                Response.ContentType = "text/plain/pdf";
+                Response.Flush();
+                Response.TransmitFile(File.FullName);
+                Response.End();
+            }
+            else
+            {
+                Lable1.Text = "Requested File is Not Available";
+            }
+        }
+    }
+}
