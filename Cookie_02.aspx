1d223d14149120d81e2bf53e118699d7b6a7ff30
sivakrishna k <sivakrishna.kante@outlook.com>
2020-09-14 06:36:46 +0530

    cookie_02

---
+++ Cookie_02.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Cookie_02.aspx.cs" Inherits="Web_forms_Aplications.Cookie_02" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>cookie</title>
+</head>
+<body>
+    <h1>cookies samples</h1>
+    <form id="form1" runat="server">
+        <asp:Label ID="Lable1" runat="server" Text="Select Brand Preference"></asp:Label>
+        <br/>
+        <br/>
+        <div>
+            <asp:CheckBox ID="Apple" runat="server" Text="Apple" />
+            <br/>
+            <asp:CheckBox ID="Lenovo" runat="server" Text="Lenovo" />
+            <br/>
+            <asp:CheckBox ID="Dell" runat="server" Text="Dell" />
+            <br/>
+            <asp:CheckBox ID="Sony" runat="server" Text="Sony" />
+            <br/>
+            <asp:CheckBox ID="HP" runat="server" Text="HP" />
+            <br />
+            <br/>
+           <asp:Button ID="Submit" runat="server" OnClick="Submit_Click" Text="submit" />
+        </div>
+        <asp:Label ID="Lable2" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Cookie_02.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web_forms_Aplications
+{
+    public partial class Cookie_02 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Cookies["Computer"].Expires = DateTime.Now.AddDays(-1);
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            Lable2.Text = "";
+
+            if (Apple.Checked)
+                Response.Cookies["Computer"]["Apple"]= "Apple";
+            if (Lenovo.Checked)
+                Response.Cookies["Computer"]["Lenovo"] = "Lenovo";
+            if (Dell.Checked)
+                Response.Cookies["Computer"]["Dell"] = "Dell";
+            if (Sony.Checked)
+                Response.Cookies["Computer"]["Sony"] = "Sony";
+            if (HP.Checked)
+                Response.Cookies["Computer"]["HP"] = "HP";
+            if (Request.Cookies["Computer"].Values.ToString() != null)
+            {
+                if (Request.Cookies["Computer"]["Apple"] != null)
+                    Lable2.Text += Request.Cookies["Computer"]["Apple"] + ", ";
+
+                if (Request.Cookies["Computer"]["Lenovo"] != null)
+                    Lable2.Text += Request.Cookies["Computer"]["Lenovo"] + ", ";
+
+                if (Request.Cookies["Computer"]["Dell"] != null)
+                    Lable2.Text += Request.Cookies["Computer"]["Dell"] + ", ";
+
+                if (Request.Cookies["Computer"]["Sony"] != null)
+                    Lable2.Text += Request.Cookies["Computer"]["Sony"] + ", ";
+
+                if (Request.Cookies["Computer"]["HP"] != null)
+                    Lable2.Text += Request.Cookies["Computer"]["HP"] + ". ";
+
+            }
+            else
+            {
+                Lable2.Text = "Please select your choice";
+            }
+            Response.Cookies["Computer"].Expires = DateTime.Now.AddDays(-1);
+        }
+    }
+}
