47b3bfe19819616807db91291d42e2b4bb02320a
sivakrishna k <sivakrishna.kante@outlook.com>
2020-09-14 14:39:38 +0530

    DataGrid

---
+++ DataGrid.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DataGrid.aspx.cs" Inherits="Web_forms_Aplications.DataGrid" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <p>The Data Grid Contains Data Table Records</p>
+            <asp:DataGrid ID="DataGrid1" runat="server"></asp:DataGrid>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DataGrid.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web_forms_Aplications
+{
+    public partial class DataGrid : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("ID");
+            table.Columns.Add("Name");
+            table.Columns.Add("Email");
+            table.Rows.Add("101", "sivakrishna", "sivakrishna@gmail.com");
+            table.Rows.Add("102", "krishna", "krishna@gmail.com");
+            table.Rows.Add("103", "ram", "ram@gmail.com");
+            table.Rows.Add("104", "ravi", "ravi@gmail.com");
+            DataGrid1.DataSource = table;
+            DataGrid1.DataBind();
+        }
+    }
+}
